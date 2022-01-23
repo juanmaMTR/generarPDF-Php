331831d98a8c4056cb4ecac877a55a4ceb8f7431
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-01-23 19:27:56 +0100

    Saca los datos de la base de datos en una tabla

--- php/generarPdf.php
+++ php/generarPdf.php
@@ -1,23 +1,28 @@
 <?php
     require 'operaciones_bd.php';//LLamo al archivo que conecta con la base de datos
     $operaciones=new Operaciones();
-    $titulos= $operaciones->consultar('SELECT');
+    $titulos= $operaciones->consultar('SELECT COLUMN_NAME FROM `empleados`.`COLUMNS`');
     $resultado=$operaciones->consultar('SELECT * FROM empleados;');
     require '../fpdf/fpdf.php';//LLamo al archivo que genera el pdf
-    //require '../fpdf/mysql_table.php';
-    $pdf=new FPDF();
+    $pdf=new FPDF(); //Lo instancio para utilizar sus métodos.
     $pdf->AddPage();
     $pdf->SetFont('Arial','B',16);
     $pdf->Cell(0,0,'PDF de la base de datos de Empleados',0,0,'C');
-    $pdf->Ln();
+    $pdf->Ln('30');
 
+    /*foreach($titulos as $titulo){
+        foreach($titulo as $fila_titulo){
+            $pdf->Cell(30,12,$fila_titulo,1);
+        }
+    }*/
+    //$pdf->Cell(30,12,$titulos,1);
 
     //Saco los datos de la base de datos en una tabla
-    foreach($resultado as $row) {
-        $pdf->SetFont('Arial','',12);	
+    foreach($resultado as $fila) {
+        $pdf->SetFont('Arial','',12);
         $pdf->Ln();
-        foreach($row as $column)
-            $pdf->Cell(30,12,$column,1);
+        foreach($fila as $columna)
+            $pdf->Cell(30,12,$columna,1);
     }
 
 
